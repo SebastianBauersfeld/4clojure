068da3fe9d005ebf2e58a7bc63f7b64381c6d930
Sebastian Bauersfeld <sebastianbauersfeld@gmx.de>
2013-07-19 20:05:59 -0400

    implementation of map and reduce

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -47,13 +47,13 @@
             (= (akey w1) (akey w2)))
           (akey [w]
             (reduce #(assoc %1 %2 (if-let [val (%1 %2)] (inc val) 1)) {} w))]
-          (set
-            (map val 
-                 (filter #(> (count (val %)) 1) 
-                         (reduce
-                           #(assoc %1 (akey %2) (if-let [s (%1 (akey %2))] (conj s %2) #{%2}))
-                           {}
-                           words))))))
+    (set
+      (map val 
+           (filter #(> (count (val %)) 1) 
+                   (reduce
+                     #(assoc %1 (akey %2) (if-let [s (%1 (akey %2))] (conj s %2) #{%2}))
+                     {}
+                     words))))))
 
 
 ; #78 Reimplement Trampoline
@@ -63,3 +63,88 @@
               (recur (f))
               f))]
     (invoke (apply f args))))
+
+
+; # 102 intoCamelCase
+(defn intoCamelCase [s]
+  (letfn [(cap [x] (cons (java.lang.Character/toUpperCase (first x)) (remove- (rest x))))
+          (remove- [x] (if (= (first x) \-)
+                         (cap (rest x)) 
+                         (if (= (first x) nil)
+                           x
+                           (cons (first x) (remove- (rest x))))))]
+    (apply str (remove- s))))
+
+
+; #custom splits a string with hyphens into a vector of substrings
+(defn mysplit [s]
+  (map
+    (partial apply str)
+    (reverse (reduce
+               #(if (= %2 \-)
+                  (cons [] %)
+                  (cons (conj (first %) %2) (rest %)))
+               (list [])
+               s))))
+
+; #custom returns all permutations of a sequence s
+(defn powerset [s]
+  (if (empty? s)
+    #{s}
+    (let [e (first s)
+          power-e (powerset (disj s e))]
+      (clojure.set/union
+        power-e
+        (set (map #(conj % e) power-e))))))
+
+
+; great (non-recursive) solution for powerset, by http://halfabrane.blogspot.com/2011/12/power-sets.html
+(comment
+  (defn power-set [s]
+    (reduce (fn [ss x] (concat ss (map #(conj % x) ss))) [#{}] s)))
+
+
+; #custom Ackermann's function
+(defn ackermann [m n]
+  (cond
+    (= m 0) (inc n)
+    (= n 0) (recur (dec m) 1)
+    :else (recur (dec m) (ackermann m (dec n)))))
+
+
+
+; #custom permutate all items in a set
+(defn permuts [s]
+  (letfn [(weave [el ls]
+            (map #(cons el %) ls))]
+    (if (seq s)
+      (apply concat (map #(weave % (permuts (disj s %))) s))
+      (list ()))))
+
+; #custom permutate all items in a list
+(defn permutl [l]
+  (letfn [(idx2l [idxstr]
+            (map #(nth l %)  idxstr))]
+    (map idx2l (permuts (set (range (count l))))))) 
+
+
+; #custom implementation of map
+(defn mymap [f xs]
+  (if (seq xs)
+    (cons (f (first xs)) (mymap f (rest xs)))
+    ()))
+
+; #custom tail-recursive implementation of map
+(defn mymaptr [f xs]
+  (letfn [(helper [xs acc]
+            (if (seq xs)
+              (recur (rest xs) (conj acc (f (first xs))))
+              acc))]
+    (seq (helper xs []))))
+
+
+; #custom implementation of reduce
+(defn myreduce [f v xs]
+  (if (seq xs)
+    (recur f (f v (first xs)) (rest xs))
+    v))
